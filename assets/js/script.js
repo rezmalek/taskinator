81dad033d8e60239cfff3d9a5b12525e68c4dca3
RezMalek <rezmalek7@gmail.com>
2020-06-29 10:36:43 -0700

    add ability to submit tasks

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,11 +1,38 @@
-const btn = document.querySelector('.btn');
+const btn = document.querySelector('#save-task');
 const tasksToDo = document.querySelector('#tasks-to-do');
+const formEl = document.querySelector('#task-form')
 
-function taskHandler() {
-    let taskItem = document.createElement('li');
-    taskItem.textContent = 'This is a new task';
-    taskItem.className = 'task-item';
-    tasksToDo.appendChild(taskItem);
+function taskFormHandler(e) {
+    e.preventDefault();
+
+    let taskNameInput = document.querySelector('input[name = "task-name"]').value;
+    let taskTypeInput = document.querySelector('select[name = "task-type"]').value;
+    if (!taskNameInput || !taskTypeInput) {
+        alert('You need to fill out the task form!');
+        return false;
+    }
+
+    formEl.reset();
+
+    let taskDataObj = {
+        name: taskNameInput,
+        type: taskTypeInput
+    }
+
+    createTaskEl(taskDataObj);
 };
 
-btn.addEventListener('click', taskHandler);
+function createTaskEl(taskDataObj) {
+    let taskItemEl = document.createElement('li');
+    taskItemEl.className = 'task-item';
+    
+    let taskInfoEl = document.createElement('div');
+    taskInfoEl.className = 'task-info';
+    
+    taskInfoEl.innerHTML = `<h3 class = "task-name">${taskDataObj.name}</h3><span class = "task-type">${taskDataObj.type}</span>`;
+
+    taskItemEl.appendChild(taskInfoEl);
+    tasksToDo.appendChild(taskItemEl);
+}
+
+btn.addEventListener('click', taskFormHandler);
